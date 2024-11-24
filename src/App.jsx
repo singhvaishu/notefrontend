53443883db946278e9568dbf6966cb8edd82c397
Vaishali Singh <94376468+singhvaishu@users.noreply.github.com>
2024-11-24 23:15:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Signup />} />
         <Route
           path="/dashboard"
           element={
